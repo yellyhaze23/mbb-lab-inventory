cf3281229eb0de486af226cdb88c66de9501636d
Ariel Longa <ariellonga7@gmail.com>
2026-02-27 13:39:58 +0800

    20 commit

--- src/constants/measurement.js
+++ src/constants/measurement.js
@@ -12,7 +12,7 @@ export const TRACKING_TYPES = {
 export const CHEMICAL_UNITS = ['mg', 'g', 'kg', 'uL', 'µL', 'mL', 'L'];
 export const CONSUMABLE_UNITS = ['pcs', 'pieces', 'tubes', 'vials', 'strips', 'sachets', 'preps', 'plates'];
 
-export const CHEMICAL_CONTAINER_TYPES = ['bottle', 'jar', 'vial', 'tube', 'bag', 'can', 'box', 'drum', 'ampoule'];
+export const CHEMICAL_CONTAINER_TYPES = ['bottle', 'jar', 'vial', 'tube', 'bag', 'can', 'box', 'drum', 'gallon'];
 export const CONSUMABLE_CONTAINER_TYPES = ['pack', 'box', 'pcs', 'set', 'bundle', 'carton', 'kit', 'sleeve'];
 
 export const CATEGORY_CONTENT_UNITS = {
